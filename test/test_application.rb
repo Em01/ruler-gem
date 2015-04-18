d1447399d8e61bd09d3c0bae6bd7776387ece10a
Em01 <emma_williams1@hotmail.co.uk>
2015-04-18 19:13:32 +0100

    bump up gem version

--- test/test_application.rb
+++ test/test_application.rb
@@ -3,19 +3,17 @@ require_relative "test_helper"
 class TestApp < Rulers::Application
 end
 
- class RulersAppTest < MiniTest::Unit::TestCase
- 	include Rack::Test::Methods
+class RulersAppTest < MiniTest::Unit::TestCase
+	include Rack::Test::Methods
 
-def app
- TestApp.new
- end
+	def app
+		TestApp.new
+	end
 
- def test_request
- get "/"
- assert last_response.ok?
- body = last_response.body
- assert body["Hello"]
- end
-
-
+	def test_request
+		get "/"
+	 	assert last_response.ok?
+	 	body = last_response.body
+	 	assert body["Hello"]
+	end
 end
